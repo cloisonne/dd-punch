13e7464ad822d2c1452eb5b0ccb9ed774b80e7ac
Tyler Luan <tylerluan@163.com>
2018-04-08 15:02:45 +0800

    Update main.lua

--- punch-in/main.lua
+++ punch-in/main.lua
@@ -19,8 +19,6 @@ for var = 1,3 do
 	
 	unlock();
 	
-	runApp('com.txy.anywhere');
-	mSleep(10000);
 	runApp('com.alibaba.android.rimet');
 	setScreenScale(true, 720, 1280);
 	mSleep(10000);
@@ -28,7 +26,6 @@ for var = 1,3 do
 	setScreenScale(false)
 	mSleep(10000)
 	closeApp('com.alibaba.android.rimet');
-	closeApp('com.txy.anywhere');
 	notify();
 	mSleep(5000);
 end
